7e4a223b332d0104f867cfb66b7b0df1ec3f17a8
Anatoliy Komarov <admin@komaroff.biz>
2022-01-07 11:11:03 +0300

    2

--- src/components/NewGallery.vue
+++ src/components/NewGallery.vue
@@ -57,7 +57,7 @@
         data-lg-size="1406-1390"
         class="gallery-item"
         :data-src="photo.img_src"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
+        data-sub-html="<h4></h4> <p> </p>"
         style="display: inline"
     >
       <img
