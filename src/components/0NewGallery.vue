336f1bd9c4cec742a70554e00876bf9531964697
Anatoliy Komarov <admin@komaroff.biz>
2022-01-08 06:31:56 +0300

    2

--- src/components/0NewGallery.vue
+++ src/components/0NewGallery.vue
@@ -5,20 +5,20 @@
       :settings="{ speed: 500, plugins: plugins }"
       :onInit="onInit"
       :onBeforeSlide="onBeforeSlide"
-      class="min-w-screen  flex flex-row justify-center items-start flex-wrap"
+      class="min-w-screen  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8 lg:gap-10 xl:gap-20"
   >
 
 
     <a v-for="photo in arr"
         data-lg-size="1406-1390"
-        class="gallery-item w-image h-88 flex flex-col justify-start items-center border-2 border-gray-200 bg-white self-straight rounded cursor-pointer "
+        class="gallery-item flex flex-col justify-start items-center border-2 border-gray-200 bg-white self-straight rounded cursor-pointer"
         :data-src="photo.img_src"
         data-sub-html="<h4></h4> <p> </p>"
         :key="photo.id"
     >
       <img
           style="object-fit:cover;"
-          class="img-responsive w-full h-full object-cover "
+          class="img-responsive w-full h-full object-cover"
           :src="photo.img_src"
           :key="photo.img_src"
       />
@@ -85,28 +85,5 @@ body {
 .gallery-item {
   margin: 5px;
 }
-.w-image{
-  width: 20%;
-  align-self: stretch;
-}
-@media(max-width:1280px){
-  .w-image {
-    width: 30%;
-  }
-}
-@media(max-width:960px){
-  .w-image {
-    width: 40%;
-  }
-}
-@media(max-width:960px){
-  .w-image {
-    width: 46%;
-  }
-}
-@media(max-width:600px){
-  .w-image {
-    width: 100%;
-  }
-}
+
 </style>
