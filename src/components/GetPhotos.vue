c468f963da45829d6e752beeaf221be93d69e9cf
Anatoliy Komarov <admin@komaroff.biz>
2022-06-18 11:24:38 +0300

    2

--- src/components/GetPhotos.vue
+++ src/components/GetPhotos.vue
@@ -1,6 +1,12 @@
 <template>
   <!--  {{ query }}-->
-  <div class="w-full lg:w-8/12 mx-auto grid grid-cols-1 md:grid-cols-3 gap-x-10 md:gap-x-20 items-end mb-6">
+  <div class="w-full lg:w-8/12 mx-auto grid grid-cols-1 md:grid-cols-4 gap-x-10 md:gap-x-20 items-end mb-6">
+    <div class="flex flex-col justify-start items-center w-full ">
+      <p class="mt-4 md:mt-0 mb-2 text-lg">Rover</p>
+      <select name="rover" v-model="rover"  class="border border-gray-200 text-xl rounded-lg w-full p-2 h-12 focus:border-4 focus:border-blue-200 outline-none">
+        <option v-for="rov in rovers"> {{rov}}</option>
+      </select>
+    </div>
     <div class="flex flex-col justify-start items-center w-full ">
       <p class="mt-4 md:mt-0 mb-2 text-lg">Number of images:</p>
       <input type="number" min="1" max="50" v-model="amount"
@@ -30,6 +36,8 @@ export default {
   emits: ['startGallery'],
   data() {
     return {
+      rovers: ['Perseverance/MARS2020','Curiosity'],
+      rover: 'Curiosity',
       today: new Date(),
       dataFromServer: [],
       amount: 1,
@@ -43,8 +51,12 @@ export default {
     }
   },
   beforeMount() {
-    this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}`
-
+    if(this.rover === 'Curiosity') {
+      this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}`
+    }
+    if(this.rover === 'Perseverance/MARS2020') {
+      this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}`
+    }
   },
   watch: {
     amount() {
@@ -58,9 +70,19 @@ export default {
     getData() {
       this.amountOfPhotos = this.amount
       if (this.amount <= 25) {
-        this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}&page=1`
+        if(this.rover === 'Curiosity') {
+          this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}&page=1`
+        }
+        if(this.rover === 'Perseverance/MARS2020') {
+          this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}&page=1`
+        }
       } else {
-        this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}`
+        if(this.rover === 'Curiosity') {
+          this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}`
+        }
+        if(this.rover === 'Perseverance/MARS2020') {
+          this.query = `https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?api_key=jQilTRWcRu4qDJtVc2NSQlqEAMCALh7zLDmzoDOT&earth_date=${this.date}`
+        }
       }
       axios.get(this.query)
           .then(response => {
